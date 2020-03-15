063b49906c81df14d38ed627ff7c058aeec9549c
Cong Zhao <congzhao1994@gmail.com>
2020-03-15 02:23:42 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
