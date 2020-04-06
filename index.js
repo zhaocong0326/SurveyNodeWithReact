1dbb915cb3f46ce074dd19cef086b89bc8d6a031
Cong Zhao <congzhao1994@gmail.com>
2020-04-06 03:18:15 -0700

    updating

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
-require("./models/user");
+require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
