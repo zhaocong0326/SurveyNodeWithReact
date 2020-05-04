58690a734326dccf06a2a5f0043c18596214fadf
Cong Zhao <congzhao1994@gmail.com>
2020-05-03 17:49:47 -0700

    completed

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
-//Development only axios helpers!
+// Development only axios helpers!
 import axios from "axios";
 window.axios = axios;
 
--- models/Survey.js
+++ models/Survey.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const recipientSchema = require("./Recipient");
+const RecipientSchema = require("./Recipient");
 
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [recipientSchema],
+  recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
   _user: { type: Schema.Types.ObjectId, ref: "User" },
