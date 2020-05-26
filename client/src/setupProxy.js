0f6d0d5069b291e8fb2a6c8ef533fa8034f07bf5
Cong Zhao <congzhao1994@gmail.com>
2020-05-26 02:34:13 -0700

    ..

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,6 +7,4 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(proxy("/auth/google", { target: "http://localhost:5000" }));
   app.use(proxy("/api/**", { target: "http://localhost:5000" }));
-  app.use(proxy("/auth/google", { target: "https://sleepy-ocean-29933.herokuapp.com/" }));
-  app.use(proxy("/api/**", { target: "https://sleepy-ocean-29933.herokuapp.com/" }));
 };
